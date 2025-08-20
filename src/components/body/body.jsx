52763c1cc6696e4c83cf271aa199cb487ed6f707
asadullah1213 <roshan23345ali@gmail.com>
2025-08-20 18:24:35 +0500

    additition

--- src/components/body/body.jsx
+++ src/components/body/body.jsx
@@ -1,43 +1,33 @@
 import React from 'react';
 import './body.css';
+import './img/Frame.png'
 
 const Body = () => {
   return (
     <>
       <main>
-        <div className="left_side">
-          <div className="heading_box">
 
-            <h1>Lorem ipsum dolor sit.</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi.</p>
-            <button type="button">learn more</button>
-          
+        <div className="body_container">
+          <div className="left_side">
+            <div className="heading_box">
+
+              <h1>OLD  IS A PRECIOUS  <span>GOLD</span></h1>
+              {/* <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi.</p> */}
+              <button type="button">learn more</button>
+
+            </div>
+
+          </div>
+          <div className="right_side">
+
+
+            <div className="img_section">
+              <img src={require('./img/Frame.png')} alt="Frame" />
+            </div>
+            
           </div>
 
         </div>
-        <div className="right_side">
-
-
-          <div className="img_section">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-            Commo Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-             Sapiente, ipsa perspiciatis? Commodi minima quas, cumque magni 
-             hic rerum a culpa repudiandae iste nam ullam earum sequi odit pr
-             ovident aperiam enim quaerat est dignissimos et! Natus, non! Rati
-             one adipisci sunt ea?di porro omnis fuga repellendus maiores magn
-             am placeat. Est ducimus error id totam dolorem quidem quaerat atqu
-             e a nesciunt architecto cum veniam sit assumenda odio corporis inc
-             idunt praesentium quasi voluptatum, ab excepturi?
-          </div>
-<svg width="301.432" height="388.93" viewBox="0 0 301.432 388.93" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <g>
-    <path d="M188.267 0C193.239 0 197.267 4.02876 197.267 9L197.267 241.685C197.267 246.656 193.239 250.685 188.267 250.685L9 250.685C4.02875 250.685 0 246.656 0 241.685L0 9C0 4.02876 4.02875 0 9 0L188.267 0Z" fill="#B2DFDB" transform="translate(320.79 230.409)" />
-    <path d="M199.329 0C204.3 0 208.329 4.02876 208.329 9L208.329 268.412C208.329 273.384 204.3 277.412 199.329 277.412L9 277.412C4.02875 277.412 0 273.384 0 268.412L0 9C0 4.02876 4.02875 0 9 0L199.329 0Z" fill="#B2DFDB" transform="translate(413.893 92.164)" />
-  </g>
-</svg>
-
-        </div>
-
 
 
       </main>
