d933fd60cf6f27678887ed7b18cc821859df530f
asadullah1213 <roshan23345ali@gmail.com>
2025-08-20 09:04:44 +0500

    3rdd

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import Header from './components/header.jsx';
-import Body from './components/body.jsx';
+import Header from './components/header/header.jsx';
+import Body from './components/body/body.jsx';
 
 function App() {
   return (
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../components_css/header.css';
+import './header.css';
 
 const Header = () => {
   return (
