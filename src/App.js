30ed2f0957fb505f47eab6619ea7e56e35115a79
asadullah1213 <roshan23345ali@gmail.com>
2025-08-21 09:11:19 +0500

    13

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import React from 'react';
 import Header from './components/header/header.jsx';
 import Body from './components/body/body.jsx';
+import Service from "./components/service/service.jsx"
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Body />
-     
+      <Service />
     </div>
   );
 }
--- src/components/service/service.jsx
+++ src/components/service/service.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
-
+import './img.avif'
 function Service() {
   return (
-    <div>Service</div>
-  )
+    <>
+    <div className="heading">
+        <h1>Lorem, ipsum.   </h1>
+    </div>
+    <div className="detail_servi">
+      <div className="left_side">
+        <img src={require('./img.avif')} alt="" />
+      </div>
+      <div className="right_side">
+        <h1><span>|| MAINTAINED</span> INDEPENDENCE </h1>
+      </div>
+    </div>
+    
+    
+    
+    
+    
+    
+    
+    
+    </>
+  );
 }
 
 export default Service
